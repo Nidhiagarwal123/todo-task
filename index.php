bddcaa55ea6f9ec78801a1714aadb34eaf61bef3
Nidhi Agarwal <31465838+Nidhiagarwal123@users.noreply.github.com>
2018-12-06 19:06:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+
+<html>
+<head> 
+<link href="style.css" rel="stylesheet">
+<title> todo application</title>
+</head>
+
+<body>
+
+<div id="myDIV" class="header">
+  <h2>My To Do List</h2>
+  <input type="text" id="myInput" placeholder="Title...">
+  <span onclick="newElement()" class="addBtn">Add</span>
+</div>
+
+<ul id="myUL">
+<li>learning php</li>
+<li>learning laravel</li>
+<li>learning vue.js</li>
+
+</ul>
+<script src="main.js"></script>
+</body>
+</html>
